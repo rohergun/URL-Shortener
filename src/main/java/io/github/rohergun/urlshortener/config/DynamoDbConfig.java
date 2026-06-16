52bdd54412bb6c7d5b043e93119d6d1d257c83fd
RohatErgun <rohatergun06@gmail.com>
2026-06-16 15:21:25 +0200

    First Commit

---
+++ src/main/java/io/github/rohergun/urlshortener/config/DynamoDbConfig.java
@@ -0,0 +1,48 @@
+package io.github.rohergun.urlshortener.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String endpoint;
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient client) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(client)
+                .build();
+    }
+}
---
+++ src/test/java/io/github/rohergun/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rohergun.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
