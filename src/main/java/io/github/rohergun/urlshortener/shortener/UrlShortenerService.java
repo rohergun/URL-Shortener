2864ee339b67caecc90bb14b4165f0dbabbcdab1
RohatErgun <rohatergun06@gmail.com>
2026-06-19 13:04:22 +0200

    feat(UrlShortenerService): add random codeGenerator method using Stringbuilder

--- src/main/java/io/github/rohergun/urlshortener/shortener/UrlShortenerService.java
+++ src/main/java/io/github/rohergun/urlshortener/shortener/UrlShortenerService.java
@@ -1,8 +1,33 @@
 package io.github.rohergun.urlshortener.shortener;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UrlShortenerService {
+import java.security.SecureRandom;
 
+@Service
+@RequiredArgsConstructor
+public class UrlShortenerService {
+    private final UrlRepository urlRepository;
+    private final UrlValidator validator;
+
+    @Value("${app.base-url}")
+    private String baseUrl;
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int CODE_LENGTH = 7;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public String shorten(String longUrl) {
+        validator.validate(longUrl);
+//        // Todo generate code
+    }
+
+    private String generateCode() {
+        StringBuilder sb = new StringBuilder(CODE_LENGTH);
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            sb.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return sb.toString();
+    }
 }
